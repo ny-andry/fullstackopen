9077814fd88229be3ea17fdfdbd65243496d9341
Ny Andry <miantsa.andr@gmail.com>
2022-12-25 19:54:48 +0300

    slick error message

--- part5/src/components/Notification.js
+++ part5/src/components/Notification.js
@@ -3,7 +3,21 @@ const Notification = ({ message }) => {
     return null;
   }
 
-  return <div className="error">{message}</div>;
+  return (
+    <div
+      className="error"
+      style={{
+        color: "white",
+        backgroundColor: "red",
+        fontSize: "20px",
+        fontWeight: "bold",
+        padding: "10px",
+        borderRadius: "4px",
+      }}
+    >
+      {message}
+    </div>
+  );
 };
 
 export default Notification;
