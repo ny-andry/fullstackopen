f31c874c1601c1ac55191c8e21c4b2b77086ebcf
Ny Andry <miantsa.andr@gmail.com>
2022-12-26 21:51:31 +0300

    axios update request

--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -13,10 +13,24 @@ const getAll = async () => {
 };
 
 const create = async (blogObj) => {
-  const config = {
-    headers: { Authorization: token },
-  };
+  const config = { headers: { Authorization: token } };
+
   const response = await axios.post(baseUrl, blogObj, config);
   return response.data;
 };
-export default { getAll, setToken, create };
+
+const update = async (blogObj, id) => {
+  const config = { headers: { Authorization: token } };
+
+  const response = await axios.put(`${baseUrl}/${id}`, blogObj, config);
+  return response.data;
+};
+
+const remove = async (id) => {
+  const config = { headers: { Authorization: token } };
+
+  const response = await axios.delete(`${baseUrl}/${id}`, config);
+  return response.data;
+};
+
+export default { getAll, setToken, create, update, remove };
