bf6327b7f8a2a0b262d1f62a71b1f8086fb03a53
Ny Andry <miantsa.andr@gmail.com>
2022-12-27 01:28:39 +0300

    sorting finished

--- part5/src/App.js
+++ part5/src/App.js
@@ -19,6 +19,7 @@ const App = () => {
   useEffect(() => {
     const fetchBlogs = async () => {
       const blogs = await blogService.getAll();
+      blogs.sort((a, b) => b.likes - a.likes);
       setBlogs(blogs);
     };
     fetchBlogs();
