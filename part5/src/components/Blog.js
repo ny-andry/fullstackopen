67d869910b23662930dfb978c0353e42de9b5b17
Ny Andry <miantsa.andr@gmail.com>
2022-12-26 21:14:30 +0300

    blog css

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,6 +1,20 @@
+const containerStyles = {
+  border: "1px solid #ccc",
+  padding: "1rem",
+};
+
+const titleStyles = {
+  fontWeight: "bold",
+};
+
+const authorStyles = {
+  marginTop: "1rem",
+};
+
 const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+  <div style={containerStyles}>
+    <h3 style={titleStyles}>{blog.title}</h3>
+    <p style={authorStyles}>{blog.author}</p>
   </div>
 );
 
