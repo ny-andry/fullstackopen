f3f4efac69bd5b8142cd0cc261df3ec20f5ee3b7
Ny Andry <miantsa.andr@gmail.com>
2022-12-27 15:47:30 +0300

    propType BlogForm

--- part5/src/components/BlogForm.js
+++ part5/src/components/BlogForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const BlogForm = ({ createBlog }) => {
   const [newTitle, setNewTitle] = useState("");
@@ -54,4 +55,7 @@ const BlogForm = ({ createBlog }) => {
   );
 };
 
+BlogForm.propTypes = {
+  createBlog: PropTypes.func.isRequired,
+};
 export default BlogForm;
