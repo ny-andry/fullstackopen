ed26aaa2111748817cf7c850351a79b5c4c02931
Ny Andry <miantsa.andr@gmail.com>
2022-12-28 18:14:50 +0300

    Blogtest finished.

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog /> test', () => {
@@ -49,5 +50,17 @@ describe('<Blog /> test', () => {
 
     expect(elementUrl).not.toBeVisible()
     expect(elementLikes).not.toBeVisible()
+  })
+
+  test('after clicking on view, the url and likes are displayed', async () => {
+    const user = userEvent.setup()
+
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const elementUrl = container.querySelector('.url')
+    expect(elementUrl).toBeVisible()
+    const elementLikes = container.querySelector('.likes')
+    expect(elementLikes).toBeVisible()
   })
 })
