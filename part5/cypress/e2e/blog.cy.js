482f8bdd3ae1bb1328350438286fb00413f31782
Ny Andry <miantsa.andr@gmail.com>
2022-12-29 21:03:50 +0300

    cypress login test

---
+++ part5/cypress/e2e/blog.cy.js
@@ -0,0 +1,17 @@
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'root',
+      username: 'root',
+      password: 'very-complicated'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function () {
+    cy.contains('username')
+    cy.contains('password')
+  })
+})
