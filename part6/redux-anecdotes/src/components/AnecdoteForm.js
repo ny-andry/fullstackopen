38d09f045145cee0a2453348b5f46eadcdf09085
Ny Andry <miantsa.andr@gmail.com>
2023-01-07 16:56:04 +0300

    axios and linking to db.json finished.

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -4,15 +4,17 @@ import {
   clearNotification,
   setNotification,
 } from "../reducers/notificationReducer";
+import anecdoteService from "../services/anecdotes";
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault();
     const anecdote = event.target.anecdote.value;
     event.target.anecdote.value = "";
-    dispatch(createAnecdote(anecdote));
+    const newAnecdote = await anecdoteService.create(anecdote);
+    dispatch(createAnecdote(newAnecdote));
     dispatch(setNotification(`You added ${anecdote}`));
     setTimeout(() => dispatch(clearNotification()), 5000);
   };
