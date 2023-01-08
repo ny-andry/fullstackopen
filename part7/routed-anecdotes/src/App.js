4cea0486e8693d1a9b71a829c431102369c473ac
Ny Andry <miantsa.andr@gmail.com>
2023-01-09 02:51:31 +0300

    About refactored

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import About from "./component/About";
 
 const Menu = () => {
   const padding = {
@@ -28,28 +29,6 @@ const AnecdoteList = ({ anecdotes }) => (
         <li key={anecdote.id}>{anecdote.content}</li>
       ))}
     </ul>
-  </div>
-);
-
-const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-
-    <em>
-      An anecdote is a brief, revealing account of an individual person or an
-      incident. Occasionally humorous, anecdotes differ from jokes because their
-      primary purpose is not simply to provoke laughter but to reveal a truth
-      more general than the brief tale itself, such as to characterize a person
-      by delineating a specific quirk or trait, to communicate an abstract idea
-      about a person, place, or thing through the concrete details of a short
-      narrative. An anecdote is "a story with a point."
-    </em>
-
-    <p>
-      Software engineering is full of excellent anecdotes, at this app you can
-      find the best and add more.
-    </p>
   </div>
 );
 
