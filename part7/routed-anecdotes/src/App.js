47c134a3a4537f4c9586823ac64eac4e5bb3b459
Ny Andry <miantsa.andr@gmail.com>
2023-01-09 02:52:54 +0300

    Menu refactored

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,25 +1,6 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import About from "./component/About";
-
-const Menu = () => {
-  const padding = {
-    paddingRight: 5,
-  };
-  return (
-    <div>
-      <Link to="/" style={padding}>
-        anecdotes
-      </Link>
-      <Link to="/create" style={padding}>
-        create new
-      </Link>
-      <Link to="/about" style={padding}>
-        about
-      </Link>
-    </div>
-  );
-};
+import Menu from "./component/Menu";
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
