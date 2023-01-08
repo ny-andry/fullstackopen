c89cbde751be5fa281ab559d6a7bd36b029c9d5f
Ny Andry <miantsa.andr@gmail.com>
2023-01-09 02:58:20 +0300

    Refactor CreateNew

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -3,55 +3,7 @@ import About from "./component/About";
 import Menu from "./component/Menu";
 import Footer from "./component/Footer";
 import AnecdoteList from "./component/AnecdoteList";
-
-const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    props.addNew({
-      content,
-      author,
-      info,
-      votes: 0,
-    });
-  };
-
-  return (
-    <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-        </div>
-        <div>
-          author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
-        </div>
-        <div>
-          url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
-        </div>
-        <button>create</button>
-      </form>
-    </div>
-  );
-};
+import CreateNew from "./component/CreateNew";
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
